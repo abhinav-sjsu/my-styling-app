28001b9a6b591855249aef6d1bb1e34147e59d6d
Abhinav Kapur <abhinav.kapur1408@gmail.com>
2024-05-21 17:44:00 -0700

    Added Button.jsx and index.css

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Button from './Button.jsx'
+
+/*
+How to style REACT components with CSS
+
+not including external frameworks like TAILWIND
+
+We will cover 
+
+1. EXTERNAL
+2. MODULES
+3. INLINE
+
+*/
+
+function App() {
+
+  return (
+    <>
+      <Button/>
+    </>
+  )
+}
+
+export default App
